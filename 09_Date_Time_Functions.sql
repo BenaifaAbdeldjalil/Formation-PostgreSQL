25d89cc6359d46e01b57a0ff9303d51149569876
ABDELDJALIL BENAIFA <77646178+BenaifaAbdeldjalil@users.noreply.github.com>
2026-02-08 16:24:51 +0100

    Update 09_Date_Time_Functions.sql

--- 09_Date_Time_Functions.sql
+++ 09_Date_Time_Functions.sql
@@ -15,7 +15,8 @@ SELECT
     NOW() AS now_timestamp,             -- Renvoie la date et l’heure actuelles (avec timezone)
     CURRENT_DATE AS current_date,        -- Renvoie uniquement la date du jour
     CURRENT_TIME AS current_time,        -- Renvoie uniquement l’heure actuelle
-    CURRENT_TIMESTAMP AS current_timestamp -- Équivalent standard SQL de NOW()
+    CURRENT_TIMESTAMP AS current_timestamp, -- Équivalent standard SQL de NOW()
+    LOCALTIMESTAMP  AS LOCALTIMESTAMP -- Équivalent standard SQL de NOW()
 ;
 
 /* Précision sur l’heure (nombre de chiffres après la virgule) */
@@ -24,7 +25,8 @@ select
     CURRENT_TIME(3) AS time_precision_3,         -- Heure avec 1 décimales
     CURRENT_TIME(0) AS time_precision_0,         -- Heure avec 3 décimales
     CURRENT_TIMESTAMP(3) AS timestamp_precision, -- Timestamp avec 3 décimales
-    CURRENT_TIMESTAMP(1) AS timestamp_precision1 -- Timestamp avec 1 décimales
+    CURRENT_TIMESTAMP(1) AS timestamp_precision1,
+    CURRENT_TIMESTAMP(2) AS timestamp_precision2 -- Timestamp avec 1 décimales
 ;
 
 /* Fonctions locales (sans timezone) */
@@ -32,15 +34,22 @@ SELECT
     LOCALTIME AS local_time,               -- Heure locale sans timezone
     LOCALTIMESTAMP AS local_timestamp,     -- Timestamp local sans timezone
     LOCALTIME(1) AS local_time_precision,  -- Heure locale avec précision
-    LOCALTIMESTAMP(1) AS local_timestamp_precision,  --2026-01-30 22:56:26.600
-    LOCALTIMESTAMP(2) AS local_timestamp_precision,  --2026-01-30 22:56:26.660
-    LOCALTIMESTAMP(3) AS local_timestamp_precision  --2026-01-30 22:56:26.664
+    LOCALTIMESTAMP(1) AS local_timestamp_precision1,  --2026-01-30 22:56:26.600
+    LOCALTIMESTAMP(2) AS local_timestamp_precision2,  --2026-01-30 22:56:26.660
+    LOCALTIMESTAMP(3) AS local_timestamp_precision3  --2026-01-30 22:56:26.664
 ;
 
 /* ==============================================================================
    2. EXTRACTION DES PARTIES D’UNE DATE
 =============================================================================== */
 ---text
+
+SELECT
+    orderid,
+    orderdate
+FROM orders;
+
+
 SELECT
     orderid,
     orderdate,
@@ -48,7 +57,16 @@ SELECT
     -- Nom du jour en toutes lettres
     TO_CHAR(orderdate, 'Month') AS nom_mois
 FROM orders;
+----
 
+SELECT
+    orderid,
+    orderdate,
+    now() as day_date,
+    TO_CHAR(now(), 'Day') AS nom_jour,
+    -- Nom du jour en toutes lettres
+    TO_CHAR(orderdate, 'Month') AS nom_mois
+FROM orders;
 
 ----numeric
 SELECT
@@ -103,7 +121,28 @@ GROUP BY EXTRACT(YEAR FROM orderdate);
 /* ==============================================================================
    4. FIN DU MOIS (équivalent EOMONTH)
 =============================================================================== */
+SELECT
 
+ DATE_TRUNC('month', NOW())
+        + INTERVAL '1 month'
+        - INTERVAL '1 day' AS fin_du_mois,
+ DATE_TRUNC('month', NOW())
+        + INTERVAL '1 month' AS un_mois_plus,
+ DATE_TRUNC('month', NOW())
+        - INTERVAL '1 day' AS un_jours_moins,
+ DATE_TRUNC('month', NOW())
+        + INTERVAL '1 year' AS une_annee_plus,
+DATE_TRUNC('month', NOW())::date AS date
+
+
+
+SELECT
+    orderid,
+    orderdate,
+    DATE_TRUNC( 'day', orderdate ) AS nom_jour
+
+    from orders;
+----------
 SELECT
     orderid,
     orderdate,
@@ -143,16 +182,12 @@ WHERE EXTRACT(MONTH FROM orderdate) = 2;
 SELECT
     orderid,
     orderdate,
-
     TO_CHAR(orderdate, 'YYYY-MM-DD') AS format_iso,
     -- Format ISO standard
-
     TO_CHAR(orderdate, 'DD-MM-YYYY') AS format_eu,
     -- Format européen
-
     TO_CHAR(orderdate, 'HH24:MI:SS') AS heure,
     -- Affichage de l’heure
-
     TO_CHAR(orderdate, 'FMDay FMMonth') AS date_lisible
     -- Affichage lisible sans espaces (FM = Fill Mode)
 FROM  orders;
@@ -162,7 +197,12 @@ FROM  orders;
 SELECT
     orderid,
     creationtime,
-    TO_CHAR(creationtime, 'YYYY-MM-DD') AS format_iso
+    TO_CHAR(orderdate, 'YYYY-MM-DD') AS format_iso
+FROM  orders;
+
+SELECT
+    orderid,
+    creationtime
 FROM  orders;
 
 
@@ -173,17 +213,17 @@ FROM  orders;
 SELECT
     CAST('2025-08-20' AS DATE) AS texte_vers_date,
     -- Convertit un texte en date
-    CAST(NOW() AS DATE) AS timestamp_vers_date
+    TO_CHAR( CAST(creationtime AS DATE), 'YYYY-MM-DD')  AS timestamp_vers_date
     -- Supprime l’heure d’un timestamp
-;
+FROM  orders;
 
 ----- alternative
 SELECT
     '2025-08-20' :: DATE as texte_vers_date,
     -- Convertit un texte en date
-    NOW():: DATE AS timestamp_vers_date
+    creationtime:: DATE AS timestamp_vers_date
     -- Supprime l’heure d’un timestamp
-;
+FROM  orders;
 
 /* ==============================================================================
    8. AJOUT / SOUSTRACTION DE DATES
